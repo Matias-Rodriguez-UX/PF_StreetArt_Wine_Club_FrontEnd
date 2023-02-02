4f5feead2b68714de266d543ac25cd07da75f9ed
David Hurtado <david.hr97110105243@gmail.com>
2023-02-02 15:56:50 -0500

    haciendo detail

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {  BrowserRouter, Route, Switch} from 'react-router-dom';
+import Detail from './components/Detail/Detail.jsx';
+/* import Home from './components/Home/Home.jsx' */
 
 function App() {
-    return (
-      <div className="App">
-        <h1>Henry PF</h1>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <BrowserRouter>
+    <div className="App">
+        <Switch>
+          {/* <Route exact path="/" component={LandingPage}/>
+          <Route exact path= '/home' component={Home}/>
+          <Route path='/videogame' component={VideogameCreate}/>
+          <Route path='/home/:id' component={Detail}/>
+          <Route path='/*' component={NotFound}/> */}
+          <Route exact path="/" component={Detail}/>
+        </Switch>
+     
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
