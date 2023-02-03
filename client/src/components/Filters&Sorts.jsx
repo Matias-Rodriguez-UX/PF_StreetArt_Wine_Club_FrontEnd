5fc3478141659fb1fe20ecba5278a0c92a10418f
Nahueabasto <nahuel7474@gmail.com>
2023-02-03 14:38:43 -0300

    compot

--- client/src/components/Filters&Sorts.jsx
+++ client/src/components/Filters&Sorts.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { useState, UseEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { orderStates } from "../Redux/Actions";
+// import React from "react";
+// import { useState, UseEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { orderStates } from "../Redux/Actions";
 
-export default function FiltersSort() {
+// export default function FiltersSort() {
 
-    const dispatch = useDispatch();
+//     const dispatch = useDispatch();
 
-    function handleSort(e) {
-        dispatch(orderStates(e.target.value));
-        setOrder(e.target.value);
-    }
+//     function handleSort(e) {
+//         dispatch(orderStates(e.target.value));
+//         setOrder(e.target.value);
+//     }
 
-    return(
+//     return(
 
-    )
-}
+//     )
+// }
