f205c17c8b34b808d31b368d8fbbc705b7100a1f
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-03 13:47:21 -0300

    change acces to memberships

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from './components/Landing';
 import Join from './components/Join'
 import Shop from './components/Shop'
+import Memberships from './components/Memberships'
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Switch>
         <Route exact path={'/'} component={Landing} />
         <Route path={'/home'} component={Home} />
-        <Route path={'/join'} component={Join} />
+        <Route exact path={'/memberships'} component={Memberships} />
+        <Route path={'memberships/join'} component={Join} />
         <Route path={'/shop'} component={Shop} />
       </Switch>
     </BrowserRouter>
