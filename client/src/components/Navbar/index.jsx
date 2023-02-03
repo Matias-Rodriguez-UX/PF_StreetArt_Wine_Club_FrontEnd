806c9d9891b84672fc784125ac059aad3f493842
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2023-02-03 18:46:07 -0300

    add(client/Shop cards, folders & dumb components to render) add(client/Login folder and file)

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,18 +1,22 @@
 import React, { useRef } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink as Link } from 'react-router-hash-link';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
-export default function NavigationBar({FAQsRef}) {
 
-    const myRef = useRef(null);
+export default function NavigationBar() {
 
-    const handleClick = () => {
-        myRef.current.scrollIntoView({ behavior: 'smooth', block: 'start'})
-    };
+    // const faqRef = useRef(null);
+
+    // const handleClick = () => {
+    //     faqRef.current.scrollIntoView({ behavior: 'smooth', block: 'start'})
+    // };
 
     return (
+        <BrowserRouter>
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
         <Navbar.Brand href='/home'>StreetArt Wine Club</Navbar.Brand>
@@ -23,17 +27,18 @@ export default function NavigationBar({FAQsRef}) {
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/shop">Shop</Nav.Link>
             <Nav.Link href="/memberships">Join</Nav.Link>
-            <Nav.Link href='#' onClick={ handleClick }>FAQs</Nav.Link>
+                <Nav.Link href='#'>FAQs</Nav.Link>
             <Nav.Link href="#">Contact</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link eventKey={2} href="/signup">
+            <Nav.Link type="button" className="btn btn-warning btn-sm" href="/login">Login</Nav.Link>
+            <Nav.Link eventKey={2} href="/signup" type="button" className="btn btn-warning btn-sm">
               Sign up
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </BrowserRouter>
     )
 }
