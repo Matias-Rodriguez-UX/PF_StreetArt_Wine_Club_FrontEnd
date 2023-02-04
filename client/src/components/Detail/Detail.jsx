b0235e806dde0e4a2ca9cc7499e244096ba55881
DavidStevens1997 <david.hr97110105243@gmail.com>
2023-02-03 19:20:14 -0500

    haciendo-detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -38,7 +38,8 @@ export default function Detail(props){
               <div className="product-description">
                 <ul>
                   <li>Grapes: </li>
-                  <li>Winreys: </li>
+                  <li>Winrey: </li>
+                  <li>Type: </li>
                   <li>State: </li>
                   <li>Regions: </li>
                 </ul>
@@ -52,7 +53,7 @@ export default function Detail(props){
             </div>
             <div className="col col-12">
               <h3>About this wine:</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <p>DETAIL Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
             </div>
           </div>
           
