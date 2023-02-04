83ad7ffbbf47927b42edf584cdbf644278a9c0c5
Matias Barroso <matibarroso10@gmail.com>
2023-02-04 16:29:04 -0300

    update links footer
    links footer nav updated

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,31 +1,21 @@
 import React from "react";
 import "./footer.css";
-import { Link } from "react-scroll";
-import Nav from 'react-bootstrap/Nav';
 
 export default function Footer() {
     return (
         <>
             <div className="container-fluid m-0 p-5 h-100 bg-dark text-white ">
                 <div className="container d-flex flex-wrap gap-3 align-items-center justify-content-center">
-                    <h1 className="fs-1 ms-5 me-5 text-center">StreetArt</h1>
+                    <h1 className="fs-1 ms-5 me-5 text-center w-25">StreetArt</h1>
                     <div className="d-flex m-1 gap-3 ms-5 me-5 align-items-center justify-content-center ">
-                    <Nav className="me-auto">
-                        <Nav.Link className='link-light' href='/home'>Home</Nav.Link>
-                        <Link to="about" spy={true} smooth={true} offset={-50} duration={500}>
-                            <Nav.Link className='link-light' href="#">About</Nav.Link>
-                        </Link>
-                        <Nav.Link className='link-light' href="/shop">Shop</Nav.Link>
-                        <Nav.Link className='link-light' href="/memberships">Join</Nav.Link>
-                        <Link to="FAQs" spy={true} smooth={true} offset={-50} duration={500}>
-                            <Nav.Link className='link-light' href='#'>FAQs</Nav.Link>
-                        </Link>
-                        <Link to="contact" spy={true} smooth={true} offset={-50} duration={500}>
-                            <Nav.Link className='link-light' href="#">Contact</Nav.Link>
-                        </Link>
-                    </Nav>
+                        <a className='text-reset text-decoration-none' href="/home">Home</a>
+                        <a className='text-reset text-decoration-none' href="/home#about">About</a>
+                        <a className='text-reset text-decoration-none' href="/shop">Shop</a>
+                        <a className='text-reset text-decoration-none' href="/join">Join</a>
+                        <a className='text-reset text-decoration-none' href="/home#FAQs">FAQs</a>
+                        <a className='text-reset text-decoration-none' href="/home#contact">Contact</a>
                     </div>
-                    <div className="d-flex gap-2 ms-5 me-5 align-items-center justify-content-center ">
+                    <div className="w-25 d-flex gap-2 ms-5 me-5 align-items-center justify-content-center ">
                         <a href="https://www.facebook.com"><img width={'25px'} src="https://cdn-icons-png.flaticon.com/512/4138/4138166.png" alt="img" /></a>
                         <a href="https://www.twitter.com"><img width={'25px'} src="https://cdn-icons-png.flaticon.com/512/4138/4138168.png" alt="img" /></a>
                         <a href="https://www.instagram.com"><img width={'25px'} src="https://cdn-icons-png.flaticon.com/512/3670/3670274.png" alt="img" /></a>
