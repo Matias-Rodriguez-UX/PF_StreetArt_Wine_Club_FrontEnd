b43424d34028fee7a35e2c810eca922e4c07d213
DavidStevens1997 <david.hr97110105243@gmail.com>
2023-02-05 11:34:52 -0500

    feat(components/WIneDetailCard): action, reducer and component done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
-/* import Landing from './components/Landing'; */
+import Landing from './components/Landing';
 import Join from './components/Join'
 import Shop from './components/Shop/Shop.jsx'
 import Detail from './components/Shop/WineDetailCard/WineDetailCard.jsx';
@@ -11,10 +11,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={'/'} component={Detail} />
+        <Route exact path={'/'} component={Landing} />
         <Route path={'/home'} component={Home} />
         <Route path={'/join'} component={Join} />
-        <Route path={'/shop'} component={Shop} />
+        <Route exact path={'/shop'} component={Shop} />
         <Route path={'/shop/:id'} component={Detail} />
       </Switch>
     </BrowserRouter>
