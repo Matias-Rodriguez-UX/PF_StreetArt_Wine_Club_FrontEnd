9620482b52e67c2f3352d7acde9d21f80586ed5a
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-07 12:37:48 -0300

    corregido estilos de las cards
    ultimo push del día de hoy

--- client/src/components/Shop/WineCard/WineCard.jsx
+++ client/src/components/Shop/WineCard/WineCard.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Card.css'
 
 const Winecards = ({ name, winery, price, image }) => {
   return (
-    <Card style={{ width: '18rem', boxShadow: ' rgba(0, 0, 0, 0.1) 0px 4px 8px', height: '32rem' }}>
+    <Card className="cardWine" style={{ width: '18rem', boxShadow: ' rgba(0, 0, 0, 0.1) 0px 4px 8px', height: '32rem' }}>
       <Card.Img variant="top" src={image} />
       <Card.Body className="d-flex flex-column align-items-center justify-content-evenly">
-        <Card.Title style={{ fontSize: "24px", textDecoration: 'none' }}>{name}</Card.Title>
-        <Card.Text className="text-center">
-          {winery}
-        </Card.Text>
-        <Card.Text className="text-center">
-          {price}
+        <Card.Title style={{ fontSize: "24px" }}><strong>{name}</strong></Card.Title>
+        {winery.map(wine => (
+          <Card.Text className="text-center mb-0" style={{ fontSize: '12px' }}>
+            {wine}
+          </Card.Text>))}
+        <Card.Text className="text-center mt-2" style={{ fontWeight: 'bold', fontSize: '16px' }}>
+          ${price}
         </Card.Text>
         <Link to="/new-route">
-          <Button variant="primary">BUY</Button>
+          <Button variant="warning"><strong>BUY</strong></Button>
         </Link>
       </Card.Body>
     </Card>
