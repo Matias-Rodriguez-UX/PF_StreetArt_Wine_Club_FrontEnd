d9b1ce07a0df97db6246ea6d669280c37d3e045b
Matias Barroso <matibarroso10@gmail.com>
2023-02-11 12:43:14 -0300

    links updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from './components/Home/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Landing from './components/Landing';
-import Shop from './components/Shop/Shop.jsx'
-import Detail from './components/Shop/WineDetailCard/WineDetailCard.jsx';
-import Memberships from './components/Memberships';
-import UserProfile from './components/UserProfile/UserProfile';
-import AdminProfile from './components/Admin/AdminProfile';
-
-
-
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Landing from "./components/Landing";
+import Shop from "./components/Shop/Shop.jsx";
+import Detail from "./components/Shop/WineDetailCard/WineDetailCard.jsx";
+import Memberships from "./components/Memberships";
+import UserProfile from "./components/UserProfile/UserProfile";
+import AdminProfile from "./components/Admin/AdminProfile";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={'/'} component={Landing} />
-        <Route path={'/home'} component={Home} />
-        <Route path={'/memberships'} component={Memberships} />
-        <Route exact path={'/shop'} component={Shop} />
-        <Route path={'/shop/:id'} component={Detail} />
-        <Route path={'/userprofile'} component={UserProfile} />
-        <Route exact path={'/admin'} component={AdminProfile} />
+        <Route exact path={"/"} component={Landing} />
+        <Route path={"/home"} component={Home} />
+        <Route path={"/memberships"} component={Memberships} />
+        <Route exact path={"/shop"} component={Shop} />
+        <Route path={"/shop/:id"} component={Detail} />
+        <Route path={"/userprofile"} component={UserProfile} />
+        <Route exact path={"/admin"} component={AdminProfile} />
       </Switch>
     </BrowserRouter>
   );
