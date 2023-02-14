954df5177b5ff4f1736399454427e89d49177c1f
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-14 14:23:18 -0300

    add loding action

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -51,6 +51,11 @@ function sortArrayZtoA(x, y) {
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case LOADING_ACTION:
+      return {
+        ...state,
+        showLoading: action.payload
+      }
     case GET_PRODUCT_BY_ID:
       return {
         ...state,
@@ -98,10 +103,10 @@ export default function reducer(state = initialState, action) {
           action.payload === ""
             ? state.allProducts
             : state.allProducts.filter((el) =>
-                el.name
-                  .split(" ")
-                  .some((el) => el.includes(action.payload.split(" ")[0]))
-              ),
+              el.name
+                .split(" ")
+                .some((el) => el.includes(action.payload.split(" ")[0]))
+            ),
       };
 
     case ADD_TO_CART:
