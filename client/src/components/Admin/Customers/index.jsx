0453e4662490793e731c82cd29446f437c343179
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-16 18:44:51 -0300

    creados formularios de usuarios para suspender y membresías creación edicion y borrado

--- client/src/components/Admin/Customers/index.jsx
+++ client/src/components/Admin/Customers/index.jsx
@@ -1,69 +1,75 @@
 import React, { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { loadingAction } from "../../../actions";
 import { getAllUsers } from "../../../actions/userActions";
+import { Loader } from "../../Loader";
+import FormUser from "./FormUser";
 
 
 export default function AdminCustomers() {
     const dispatch = useDispatch()
     const allUsers = useSelector((state) => state.users.allUsers)
-    const [userList, setUserList] = useState(allUsers);
+    const isLoading = useSelector((state) => state.products.showLoading)
+    const [userList, setUserList] = useState([]);
+    const [selectedData, setSelectedData] = useState({});
+    const [showModalEdit, setShowModalEdit] = useState(false);
 
     useEffect(() => {
         dispatch(getAllUsers())
+        dispatch(loadingAction(true))
+        setUserList(allUsers)
+    }, [isLoading, userList])
 
-    }, [])
     console.log(allUsers)
 
-    // const headers = Object.keys(allUsers[0]);
-    // headers.push("banned")
+    function handleClick(item) {
+        setSelectedData(item);
+        setShowModalEdit(true);
+    };
 
-
-    // const handleBan = id => {
-    //     const newUserList = userList.map(user => {
-    //         if (user.id === id) {
-    //             return { ...user, banned: !user.banned };
-    //         }
-    //         return user;
-    //     });
-    //     setUserList(newUserList);
-    // };
+    let headers = []
+    if (allUsers.length) {
+        headers = Object.keys(allUsers[0]);
+    }
+    console.log(headers)
 
     return (
         <>
             <h1>Customers</h1>
-            {/* <Table striped bordered hover responsive>
-                <thead>
-                    <tr>
-                        {headers.map((header, index) => (
-                            <th key={index}>{header}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {allUsers.map((item, index) => (
-                        <tr key={index} onClick={() => handleClick(item)} style={{ cursor: 'pointer' }}>
-                            {headers.map((header, subIndex) => (
-                                header === "banned" ? <td>
-                                    <input
-                                        type="checkbox"
-                                        checked={item.banned}
-                                        onChange={() => handleBan(item.id)}
-                                    />
-                                </td> :
-                                    (typeof item[header] !== "object") ?
-                                        <td key={subIndex} className="ellipsis">{item[header]}</td> :
-                                        <td key={subIndex} className="ellipsis"> {item[header].map((el, index) =>
-                                            (typeof el !== 'object') ?
-                                                <li key={index} style={{ listStyleType: 'none' }}>{el}</li> :
-                                                <li key={index} style={{ listStyleType: 'none' }}>{el.name}</li>
-                                        )}</td>
+            <div>
+                {allUsers?.length ? <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            {headers.map((header, index) => (
+                                <th key={index}>{header}</th>
                             ))}
                         </tr>
-                    ))}
-
-                </tbody>
-            </Table> */}
+                    </thead>
+                    <tbody>
+                        {allUsers.map((item, index) => (
+                            <tr key={index} onClick={() => handleClick(item)} style={{ cursor: 'pointer' }}>
+                                {headers.map((header, subIndex) => (
+                                    <td key={subIndex} className="ellipsis">{item[header]}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table> : <h2>Not user to see</h2>}
+            </div>
+            <Modal show={showModalEdit} onHide={() => setShowModalEdit(false)} >
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Product</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <FormUser selectedData={selectedData} setShowModalEdit={setShowModalEdit} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowModalEdit(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
