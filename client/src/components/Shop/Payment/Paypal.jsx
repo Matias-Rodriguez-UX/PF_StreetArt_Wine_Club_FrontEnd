59417bd293908293bab0d62e19d3ae68f064a401
DavidStevens1997 <david.hr97110105243@gmail.com>
2023-02-17 13:52:14 -0500

    funciona paypal

--- client/src/components/Shop/Payment/Paypal.jsx
+++ client/src/components/Shop/Payment/Paypal.jsx
@@ -13,6 +13,7 @@ export default function Paypal(){
     const { user, isAuthenticated } = useAuth0();
     const [storedCart, setStoredCart] = useLocalStorage("cart", []);
     const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
   
     const total = cart.reduce((acc, product) => {
       return acc + product.price * product.cartQuantity;
@@ -29,10 +30,12 @@ export default function Paypal(){
       });
     }
     const onApprove = (data, actions) => {
-      return actions.order.capture();
+      return actions.order.capture(handlePay());
     }
-    const dispatch = useDispatch();
-  
+    
+    function handlePay() {
+      console.log("el pago ha sido exitoso desde la we");
+    }
     // useEffect(() => {
     //   if (storedCart.length === 0 && cart.length > 0) {
     //     setStoredCart(cart);
@@ -129,102 +132,19 @@ export default function Paypal(){
               </div>
             </div>
     
-            <div className="mb-3">
-              <label for="address2">Address 2 <span className="text-muted">(Optional)</span></label>
-              <input type="text" className="form-control" id="address2" placeholder="Apartment or suite"/>
-            </div>
-    
             <div className="row">
               <div className="col-md-5 mb-3">
-                <label for="country">Country</label>
-                <select className="custom-select d-block w-100" id="country" required>
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
-                <div className="invalid-feedback">
-                  Please select a valid country.
-                </div>
+                <label for="phone">Phone number with country code</label>
+                <input type="text" id="phone" className="form-control" placeholder="+48 999-999-999" required/>
               </div>
               <div className="col-md-4 mb-3">
-                <label for="state">State</label>
-                <select className="custom-select d-block w-100" id="state" required>
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
-                <div className="invalid-feedback">
-                  Please provide a valid state.
-                </div>
-              </div>
-              <div className="col-md-3 mb-3">
-                <label for="zip">Zip</label>
+                <label for="zip">ZipCode</label>
                 <input type="text" className="form-control" id="zip" placeholder="" required/>
                 <div className="invalid-feedback">
                   Zip code required.
                 </div>
               </div>
             </div>
-            <hr className="mb-4"/>
-            <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="same-address"/>
-              <label className="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-            </div>
-            <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="save-info"/>
-              <label className="custom-control-label" for="save-info">Save this information for next time</label>
-            </div>
-            <hr className="mb-4"/>
-    
-            <h4 className="mb-3">Payment</h4>
-    
-            <div className="d-block my-3">
-              <div className="custom-control custom-radio">
-                <input id="credit" name="paymentMethod" type="radio" className="custom-control-input" required/>
-                <label className="custom-control-label" for="credit">Credit card</label>
-              </div>
-              <div className="custom-control custom-radio">
-                <input id="debit" name="paymentMethod" type="radio" className="custom-control-input" required/>
-                <label className="custom-control-label" for="debit">Debit card</label>
-              </div>
-              <div className="custom-control custom-radio">
-                <input id="paypal" name="paymentMethod" type="radio" className="custom-control-input" required/>
-                <label className="custom-control-label" for="paypal">PayPal</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6 mb-3">
-                <label for="cc-name">Name on card</label>
-                <input type="text" className="form-control" id="cc-name" placeholder="" required/>
-                <small className="text-muted">Full name as displayed on card</small>
-                <div className="invalid-feedback">
-                  Name on card is required
-                </div>
-              </div>
-              <div className="col-md-6 mb-3">
-                <label for="cc-number">Credit card number</label>
-                <input type="text" className="form-control" id="cc-number" placeholder="" required/>
-                <div className="invalid-feedback">
-                  Credit card number is required
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-3 mb-3">
-                <label for="cc-expiration">Expiration</label>
-                <input type="text" className="form-control" id="cc-expiration" placeholder="" required/>
-                <div className="invalid-feedback">
-                  Expiration date required
-                </div>
-              </div>
-              <div className="col-md-3 mb-3">
-                <label for="cc-cvv">CVV</label>
-                <input type="text" className="form-control" id="cc-cvv" placeholder="" required/>
-                <div className="invalid-feedback">
-                  Security code required
-                </div>
-              </div>
-            </div>
-            <hr className="mb-4"/>
-            <button className="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
         </form>
         </div>
       </div>
