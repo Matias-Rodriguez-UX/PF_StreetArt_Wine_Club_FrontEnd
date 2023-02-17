5c8300617756dc00f73d1b636fdf39e6f037dd3b
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-17 16:09:47 -0300

    modificado loadings

--- client/src/components/Admin/Memberships/index.jsx
+++ client/src/components/Admin/Memberships/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Button, Modal, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { loadingAction } from "../../../actions";
 import { getMemberships } from "../../../actions/membershipsActions";
 import FormMembershipEdit from "./FormMembershipEdit";
 import FormMembershipPost from "./FormMembershipPost";
+import { Loader } from '../../Loader'
 
 
 export default function AdminMemberships() {
@@ -13,9 +15,11 @@ export default function AdminMemberships() {
     const [showModalPost, setShowModalPost] = useState(false);
     const [selectedData, setSelectedData] = useState({});
     const [showModalEdit, setShowModalEdit] = useState(false);
+    const showLoading = useSelector((state) => state.products.showLoading)
 
     useEffect(() => {
         dispatch(getMemberships())
+        dispatch(loadingAction(true))
     }, [membershipList])
 
     let headers = []
@@ -35,56 +39,59 @@ export default function AdminMemberships() {
 
     return (
         <>
-            <div className='container d-flex align-items-center justify-content-evenly mt-4'>
-                <h1>Memberships</h1>
-                <Button variant='warning' hover onClick={(e) => handleClickPost(e)}>Add Memberships</Button>
-            </div>
-            <div>
-                {allMemberships?.length ? <Table striped bordered hover responsive>
-                    <thead>
-                        <tr>
-                            {headers.map((header, index) => (
-                                <th key={index}>{header}</th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {allMemberships.map((item, index) => (
-                            <tr key={index} onClick={() => handleClick(item)} style={{ cursor: 'pointer' }}>
-                                {headers.map((header, subIndex) => (
-                                    <td key={subIndex} className="ellipsis">{item[header]}</td>
+            {showLoading ? <Loader /> : <>
+                <div className='container d-flex align-items-center justify-content-evenly mt-4'>
+                    <h1>Memberships</h1>
+                    <Button variant='warning' hover onClick={(e) => handleClickPost(e)}>Add Memberships</Button>
+                </div>
+                <div>
+                    {allMemberships?.length ? <Table striped bordered hover responsive>
+                        <thead>
+                            <tr>
+                                {headers.map((header, index) => (
+                                    <th key={index}>{header}</th>
                                 ))}
                             </tr>
-                        ))}
-                    </tbody>
-                </Table> : <h2>Not memberships to see</h2>}
-            </div>
-            <Modal show={showModalPost} onHide={() => setShowModalPost(false)} >
-                <Modal.Header closeButton>
-                    <Modal.Title>Create Membership</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <FormMembershipPost setShowModalPost={setShowModalPost} setmembershipList={setmembershipList} membershipList={membershipList} />
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setShowModalPost(false)}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-            <Modal show={showModalEdit} onHide={() => setShowModalEdit(false)} >
-                <Modal.Header closeButton>
-                    <Modal.Title>Edit Memberships</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <FormMembershipEdit selectedData={selectedData} setShowModalEdit={setShowModalEdit} />
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setShowModalEdit(false)}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                        </thead>
+                        <tbody>
+                            {allMemberships.map((item, index) => (
+                                <tr key={index} onClick={() => handleClick(item)} style={{ cursor: 'pointer' }}>
+                                    {headers.map((header, subIndex) => (
+                                        <td key={subIndex} className="ellipsis">{item[header]}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table> : <h2>Not memberships to see</h2>}
+                </div>
+                <Modal show={showModalPost} onHide={() => setShowModalPost(false)} >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create Membership</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <FormMembershipPost setShowModalPost={setShowModalPost} setmembershipList={setmembershipList} membershipList={membershipList} />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShowModalPost(false)}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={showModalEdit} onHide={() => setShowModalEdit(false)} >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Memberships</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <FormMembershipEdit selectedData={selectedData} setShowModalEdit={setShowModalEdit} />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShowModalEdit(false)}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+            }
         </>
     )
 }
