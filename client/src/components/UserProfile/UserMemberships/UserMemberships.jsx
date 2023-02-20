13ef6077efbfcaa9215e02c9241a081be74ec2f4
JaviPeder~ <javierpedernera@gmail.com>
2023-02-20 13:47:34 -0300

    Se crea el componente membership del usuario

--- client/src/components/UserProfile/UserMemberships/UserMemberships.jsx
+++ client/src/components/UserProfile/UserMemberships/UserMemberships.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInfo } from '../../../actions/userActions';
+import "./UserMemberships.css"
 
 export default function UserOrders (){
-    return (
-        <h1>My memberships</h1>
-    )
+
+    const dispatch = useDispatch()
+    const userActive = useSelector(state => state.users.userInfo)
+    const email = sessionStorage.getItem('user');
+
+    // useEffect (()=> {
+    //     if(userActive){
+    //        dispatch(getUserInfo(email))  
+    //     }
+    //     getUserMembership()
+    // },[dispatch]) 
+console.log(userActive)
+    return (<div className='box'>
+        {userActive.membership?(
+        <Card className="text-center">
+          <Card.Header>{userActive.fullname}</Card.Header>
+          <Card.Body>
+            <Card.Title>{userActive.membership.name.toUpperCase()}</Card.Title>
+            <Card.Text>
+            {userActive.membership.description}
+            </Card.Text>
+            <Button className="btn btn-warning btn-lg">Update</Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">{userActive.email} tu membresia está vigente desde el {userActive.membership.purchaseDate}</Card.Footer>
+        </Card>): <div> Aun no tienes membresias</div>}</div>
+      );
 };
