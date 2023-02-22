2d70aa9c97aa4798a6db1463c081df9def975584
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-22 12:02:02 -0300

    corregido altas de las membresías

--- client/src/components/Shop/WineDetailCard/WineDetailCard.jsx
+++ client/src/components/Shop/WineDetailCard/WineDetailCard.jsx
@@ -200,7 +200,7 @@ export default function Detail(props) {
                     <button type="button" id="button-cart" className="btn btn-warning btn-lg mt-2 ms-4" onClick={() => handleClick(wine.id, cartQuantity, wine.name, wine.price)}>Add to cart <i class="bi bi-cart-check-fill"></i></button>
                   </div>
                   :
-                  <div className="d-flex align-items-end justify-content-center gap-3" >
+                  <div className="d-flex align-items-end justify-content-center gap-3 ms-4" >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#df4759" className="bi bi-exclamation-circle" viewBox="0 0 16 16">
                       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                       <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
