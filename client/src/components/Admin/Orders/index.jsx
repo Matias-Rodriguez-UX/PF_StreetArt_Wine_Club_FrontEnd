321d5a2053e6183f309dfcdb8ec69b6dd663d9e3
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-24 18:55:20 -0300

    feat(src/components/Admin/Orders) filters working

--- client/src/components/Admin/Orders/index.jsx
+++ client/src/components/Admin/Orders/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadingAction } from "../../../actions";
-import { getOrders } from "../../../actions/ordersAction";
+import { filterOrderByStatus, getOrders } from "../../../actions/ordersAction";
 import { Loader } from '../../Loader/index'
 import WebPagination from "../../Shop/Pagination/Pagination";
 import FiltersOrders from "./FiltersOrders";
@@ -11,14 +11,14 @@ import TableOrders from "./TableOrders";
 export default function AdminOrders() {
     const dispatch = useDispatch()
     const showLoading = useSelector((state) => state.products.showLoading)
-    const allOrders = useSelector((state) => state.orders.allOrders)
+    const allOrders = useSelector((state) => state.orders.orders)
     const [orders, setOrders] = useState(allOrders);
 
     const [currentPage, setCurrentPage] = useState(1);
     const [ordersPerPage, setOrdersPerPage] = useState(5);
     const indexOfLastOrder = currentPage * ordersPerPage;
     const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
-    const currentOrders = orders.slice(indexOfFirstOrder, indexOfLastOrder);
+    const currentOrders = allOrders.slice(indexOfFirstOrder, indexOfLastOrder);
     const pagination = (pageNumber) => {
         setCurrentPage(pageNumber)
     };
@@ -27,12 +27,16 @@ export default function AdminOrders() {
         dispatch(getOrders())
     }, [dispatch, orders])
 
+    function handleFilterStatus(e) {
+        e.preventDefault()
+        dispatch(filterOrderByStatus(e.target.value))
+    }
 
     return (
         <>
             <div className='container d-flex align-items-center justify-content-evenly mt-3 bg-dark'>
                 <h1 className="text-white">Orders</h1>
-                <FiltersOrders allOrders={allOrders} setOrders={setOrders} />
+                <FiltersOrders handleFilterStatus={handleFilterStatus} />
             </div>
             {showLoading ? <div className="container col py-5 mt-5"> <Loader /> </div> :
                 <div className="mt-3 mb-3">
