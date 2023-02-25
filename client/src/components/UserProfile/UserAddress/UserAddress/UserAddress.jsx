3e33a2961f146365af9bbb97e27bef199b18cf77
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2023-02-25 13:25:40 -0300

    edit address

--- client/src/components/UserProfile/UserAddress/UserAddress/UserAddress.jsx
+++ client/src/components/UserProfile/UserAddress/UserAddress/UserAddress.jsx
@@ -2,18 +2,31 @@ import React, { useEffect, useState } from "react";
 import Form from 'react-bootstrap/Form';
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { getStates } from "../../../actions";
-import { createUserAddress, getAllCities } from "../../../actions/userActions";
-import UserShowAddress from "./UserShowAddress";
+import { getStates } from "../../../../actions";
+import { createUserAddress, deleteUserAddress, getAllCities } from "../../../../actions/userActions";
+import { getUserAddresses } from "../../../../actions/userActions";
+import { Row, Col, Card } from 'react-bootstrap';
+import { Toast } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './address.css';
+// import logo from '../../../../public/favicon.ico';
+
 
 export default function UserAddress(){
     const dispatch = useDispatch();
     const history = useHistory();
+    
+    const userInfo = useSelector((state) => state.users.userInfo);
     const states = useSelector((state) => state.products.states);
     const cities = useSelector((state) => state.users.cities);
+    const addresses = useSelector(state => state.users.userAddresses);
+    console.log(addresses);
 
-    const userInfo = useSelector((state) => state.users.userInfo);
-// console.log(userInfo);
+    const [shouldRender, setShouldRender] = useState(false);
+    const [showToast, setShowToast] = useState(false);
+    const [showToastSubmit, setShowToastSubmit] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [addressToEdit, setAddressToEdit] = useState(null);
     const [ input, setInput ] = useState({
         reference: '',
         address: '',
@@ -23,8 +36,10 @@ export default function UserAddress(){
         region: ""
     });
     // console.log(states);
-    input.userEmail = userInfo.email;
-    console.log(input);
+    input.userEmail = userInfo.email; 
+    const toggleShowA = () => setShowToast(!showToast);
+    const toggleShowSubmit = () => setShowToastSubmit(!showToastSubmit);
+
     let orderedStates = states.sort(function(a,b) {
         if (a.name > b.name){
             return 1;
@@ -34,10 +49,9 @@ export default function UserAddress(){
         }
         return 0;
     });
-    // console.log(orderedStates);
-
-
-      let orderedCities = cities.municipios.sort(function(a,b) {
+    //   console.log(orderedStates);
+   
+    let orderedCities = cities.municipios?.sort(function(a,b) {
         if (a.nombre > b.nombre){
             return 1;
         }
@@ -49,6 +63,7 @@ export default function UserAddress(){
 
     useEffect(() => {
         dispatch(getStates());
+        dispatch(getUserAddresses(userInfo.email))
     }, [dispatch]);
 
     const handleChange = (e) => {
@@ -56,13 +71,11 @@ export default function UserAddress(){
             ...input,
             [e.target.name]: e.target.value
         });
-        // console.log(input);
+       
     };
-
+//  console.log(input);
     const handleSelect =  (e) => {
-        if (e.target.name) {
-            console.log(e.target.name);
-            console.log(e.target.value);
+        if (e.target.name === 'state') {
              dispatch(getAllCities(e.target.value));
             setInput({
                 ...input,
@@ -70,10 +83,11 @@ export default function UserAddress(){
             });
         }
     };
-  
+    // console.log(orderedCities);
+
     const handleCitySelect =  (e) => {
-        if (e.target.name) {
-            console.log(e.target.name);
+        if (e.target.name === 'region') {
+            // console.log(e.target.name);
             console.log(e.target.value);
             setInput({
                 ...input,
@@ -81,28 +95,93 @@ export default function UserAddress(){
             });
         }
     };
+    console.log(input);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(input);
         if (input.reference === '' || input.address === '' || input.zipCode === 0 || input.telephone === 0) 
         return alert('You need to complete all the fields');
-
         dispatch(createUserAddress(input));
-        alert ('Address added!');
+        toggleShowSubmit();
         setInput({
             reference: '',
             address: '',
-            zipCode: "",
-            telephone: ""
+            zipCode: '',
+            telephone: ''
         });
-        history.push('/userprofile');
+        window.location.reload();
     };
+
+    const handleDelete = (e, el) => {
+        e.preventDefault()
+        dispatch(deleteUserAddress(el.id));
+        toggleShowA();
+        window.location.reload();
+    };
+
+    const handleEditClick = (address) => {
+        console.log(address);
+        setAddressToEdit(address);
+        setShowEditModal(true);
+      };
     
     return (
-        <>
         <div className="container col py-5 mt-5" display='flex'>
-          <div class="col-md-8">
+          <div class="col-md-9">
+          <Row>
+                {
+                    (typeof addresses !== 'string') ?
+                addresses.map((el, index) => 
+                    ( 
+                    <>
+                    <Col key={index} xs={12} md={4} className="mb-5 me-5">
+                    <Card className='me-5' border="warning" style={{ width: '15rem' }}>
+                    <Card.Body>
+                    <Form.Check 
+                            type="switch"
+                            id="custom-switch"
+                            label=""
+                            /> 
+                      <Card.Title>                            
+                        {el.reference}
+                     </Card.Title>
+                      <Card.Text className='justify-content-center'>
+                        {el.address}
+                        <br/>
+                        {el.telephone}
+                        <br/>
+                        {el.state}
+                        <br/>
+                        {el.region}
+                      </Card.Text> 
+                      <div class='d-flex justify-content-end'>  
+                      <div>                   
+                        <button class="btn btn-warning btn-sm me-2" onClick={() => handleEditClick(el)}>Edit</button>
+                        </div>
+                        <div>
+                        <button class="btn btn-warning btn-sm" onClick={(e) => handleDelete(e, el)}>x</button>
+                        {showToast && (
+                            <Toast className='toast prefers-reduced-motion: no-preference' show={showToast} onClose={toggleShowA}  delay={100} autohide>
+                            <Toast.Header>
+                                <img src='holder.js/20x20?text=%20' className="rounded me-2" alt="brand logo" />
+                                <strong className="me-auto">Street Art Wines Club</strong>
+                                <small>just now</small>
+                            </Toast.Header>
+                            <Toast.Body>Woohoo, you deleted that address!</Toast.Body>
+                            </Toast>
+                        )}
+                        </div>
+                    </div>
+                    </Card.Body>
+                    </Card>
+                    </Col>
+                    <br />  
+                    </> 
+                )): 
+                    <div className="address"><p>You don't have registered addresses yet</p></div>
+                    }
+                    </Row> 
                         <div class="card">
                             <div class="card-body">
                                 <h4>Add address</h4>
@@ -144,7 +223,7 @@ export default function UserAddress(){
 
                                 <div class="row mb-3">
                                     <div class="col-sm-9 text-secondary">
-                                        <Form.Select name='region' onChange={(e) => handleCitySelect(e)} >
+                                        <Form.Select name='region' onChange={(e) => handleCitySelect(e)}>
                                             <option name='region'>City</option>
                                             {(orderedCities ? orderedCities.map((el, index) => <option key={index} value={el.nombre}>{el.nombre}</option>) : <div>'Error'</div>)}
                                         </Form.Select>
@@ -165,14 +244,24 @@ export default function UserAddress(){
                                     <div class="col-sm-9 text-secondary">
                                         <input type="button" class="btn btn-warning btn-sm" value="Add" onClick={(e) => handleSubmit(e)}/>
                                     </div>
+                                        {showToastSubmit && (
+                                        <Toast className='toast prefers-reduced-motion: no-preference' show={showToastSubmit} onClose={toggleShowSubmit}  delay={150} autohide>
+                                        <Toast.Header>
+                                            <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                                            <strong className="me-auto">Street Art Wines Club</strong>
+                                            <small>just now</small>
+                                        </Toast.Header>
+                                        <Toast.Body>Woohoo, you added that address!</Toast.Body>
+                                        </Toast>
+                                        )}
                                     </div>
                                 </div> 
                                 </div>
                                 </div>
                                 </div>
                                 </div>
-                                </>
 
 
     )
-}
+};
+
