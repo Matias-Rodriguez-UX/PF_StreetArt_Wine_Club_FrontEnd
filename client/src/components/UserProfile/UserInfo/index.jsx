09d03b17b5be1195f878aef7261499bbdcc109ea
JaviPeder~ <javierpedernera@gmail.com>
2023-02-25 15:08:28 -0300

    se ven y actualizan los datos del user

--- client/src/components/UserProfile/UserInfo/index.jsx
+++ client/src/components/UserProfile/UserInfo/index.jsx
@@ -8,8 +8,9 @@ export default function UserInfo({ userName, setCurrentPage }) {
   // const dispatch = useDispatch();
   // const { isAuthenticated: auth, user } = useAuth0();
   const userInfo = useSelector((state) => state.users.userInfo);
-  // console.log(userInfo)
-
+  console.log(userInfo)
+  const addressSelect = userInfo.addresses.length - 1
+  console.log(addressSelect)
   // let userEmail = '';
   // if(auth){
   //   userEmail = user.email
@@ -54,25 +55,34 @@ export default function UserInfo({ userName, setCurrentPage }) {
                 <h6 class="mb-0">Phone</h6>
               </div>
               <div class="col-sm-9 text-secondary">
-                (239) 816-9029
+              {userInfo.addresses.length?userInfo.addresses[addressSelect].telephone:"You don't have registered telephone yet"}
               </div>
             </div>
-            <hr />
-            <div class="row">
-              <div class="col-sm-3">
-                <h6 class="mb-0">Mobile</h6>
-              </div>
-              <div class="col-sm-9 text-secondary">
-                (320) 380-4539
-              </div>
-            </div>
-            <hr />
+           <hr />
             <div class="row">
               <div class="col-sm-3">
                 <h6 class="mb-0">Address</h6>
               </div>
               <div class="col-sm-9 text-secondary">
-                Bay Area, San Francisco, CA
+                {userInfo.addresses.length?userInfo.addresses[addressSelect].address:"You don't have registered addresses yet"}
+              </div>
+            </div>
+            <hr />
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">State</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {userInfo.addresses.length?userInfo.addresses[addressSelect].state:""}
+              </div>
+            </div>
+            <hr />
+            <div class="row">
+              <div class="col-sm-3">
+                <h6 class="mb-0">City</h6>
+              </div>
+              <div class="col-sm-9 text-secondary">
+              {userInfo.addresses.length?userInfo.addresses[addressSelect].region:""}
               </div>
             </div>
             <hr />
