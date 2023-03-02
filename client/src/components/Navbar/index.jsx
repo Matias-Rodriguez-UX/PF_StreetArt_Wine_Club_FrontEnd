93e58aa9b94b285d1c2c7ad145ca7d96151be2c2
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-03-02 11:32:08 -0300

    Update index.jsx

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -18,19 +18,14 @@ export default function NavigationBar() {
   let location = useLocation();
   const cart = useSelector(state => state.products.cart)
   const userInfo = useSelector(state => state.users.userInfo);
-  const [cartItemsCount, setCartItemsCount] = useState(0);
+
 
   useEffect(() => {
     if (userInfo.id && isAuthenticated) {
-      dispatch(getUserCart(userInfo.id)).then(() => {
-        const itemsCount = cart?.length
-        setCartItemsCount(itemsCount);
-      })
-    } else {
-      const itemsCount = cart?.length
-      setCartItemsCount(itemsCount)
+      dispatch(getUserCart(userInfo.id))
     }
-  }, [dispatch])
+
+  }, [dispatch, cart])
 
 
   return (
@@ -60,9 +55,9 @@ export default function NavigationBar() {
             {location.pathname !== '/payment' ? <Link to="/cart" className="d-flex text-reset align-items-center justify-content-center m-0 p-0 text-decoration-none border border-0 bg-transparent me-5"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
             </svg>
-              {cartItemsCount > 0 &&
+              {cart?.length > 0 &&
                 <div className="quan-cart-i">
-                  <p style={{ fontSize: "10px" }} >{cartItemsCount}</p>
+                  <p style={{ fontSize: "10px" }} >{cart.length}</p>
                 </div>}
             </Link> : undefined
             }
